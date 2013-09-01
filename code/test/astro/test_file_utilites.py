f1ed4f633be2a3ba357f1edd2b4f3367d9e57d6f
Nick Loadholtes <nick@ironboundsoftware.com>
2013-09-01 14:03:13 -0400

    Skipping this test, might need to remove it

--- code/test/astro/test_file_utilites.py
+++ code/test/astro/test_file_utilites.py
@@ -1,7 +1,9 @@
 from nose.tools import assert_equal
+from nose import SkipTest
 
 from astro.file_utilites import load_orbital_parameters
 
+@SkipTest
 def test_load_orbital_parameters():
     assert_equal(1,2)
     
