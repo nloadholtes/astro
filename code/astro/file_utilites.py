98e7e3450a076ef8f1232ab11fdbd4bc0cfd00ef
Nick Loadholtes <nick@ironboundsoftware.com>
2014-11-07 22:48:27 -0500

    First pass at parsing a file

--- code/astro/file_utilites.py
+++ code/astro/file_utilites.py
@@ -1,4 +1,26 @@
 
 
 def load_orbital_parameters(filename):
-    pass
+    elements = {}
+    with open(filename, 'r') as f:
+        lines = f.read().split('\n')
+        reading = False
+        for line in lines:
+            chunks = line.split(' ')
+            if chunks[0] == '$$SOE':
+                print("Start seen")
+                reading = True
+                continue
+            if chunks[0] == '$$EOE':
+                print('End seen')
+                reading = False
+                break
+            if reading:
+                print('Going to try and read: %s' % chunks)
+                continue
+    return elements
+
+
+if __name__ == '__main__':
+    elements = load_orbital_parameters('ephemeris/triton_horizons.cgi.txt')
+    print(elements)
